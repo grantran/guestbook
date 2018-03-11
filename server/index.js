0164f5ca5d650fedee1d11a8392dc41bab65a6ca
grantran <grant.tran@gmail.com>
2018-03-11 11:35:35 -0700

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 8000;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 
